fd163e3bdb27e8115191d3ad5f36af2e424f7dd7
BadisG <110173477+BadisG@users.noreply.github.com>
2022-07-29 16:17:31 +0200

    Update Boutique.java

--- projetJeu/src/main/java/fr/projetjeu/model/Boutique.java
+++ projetJeu/src/main/java/fr/projetjeu/model/Boutique.java
@@ -1,22 +1,24 @@
-package PartieMahmoud;
+package fr.projetjeu.model;
 
-public class Boutique extends Events{
-private String nom;
+public class Boutique extends Events {
+	private String nom;
 
-public String getNom() {
-	return nom;
-}
-public void setNom(String nom) {
-	this.nom = nom;
-}
+	public String getNom() {
+		return nom;
+	}
 
-public void achatObjets(Events e, Inventaire i,int idx) {
-	//idx c'est l'indice de l'objet a acheter de la boutique
-	i.getListeObjetInventaire().add(e.getListeObjetEvent().get(idx));
-}
-public void venteObjets(Inventaire i, int idx) {
-	//idx c'est l'indice de l'objet a vendre a la boutique
-	i.getListeObjetInventaire().remove(idx);
-	///////////////
-}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public void achatObjets(Events e, Inventaire i, int idx) {
+		// idx c'est l'indice de l'objet a acheter de la boutique
+		i.getListeObjetInventaire().add(e.getListeObjetEvent().get(idx));
+	}
+
+	public void venteObjets(Inventaire i, int idx) {
+		// idx c'est l'indice de l'objet a vendre a la boutique
+		i.getListeObjetInventaire().remove(idx);
+
+	}
 }
