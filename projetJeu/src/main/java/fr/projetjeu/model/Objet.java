150fe02d07c6478be48db95e3f69c31a6609ac10
MAHMOUDMOUSSAOUI98 <74258199+MAHMOUDMOUSSAOUI98@users.noreply.github.com>
2022-07-29 14:45:19 +0200

    Add files via upload
    v1

--- projetJeu/src/main/java/fr/projetjeu/model/Objet.java
+++ projetJeu/src/main/java/fr/projetjeu/model/Objet.java
@@ -1,30 +1,35 @@
-package fr.projetjeu.model;
+package PartieMahmoud;
 
 public class Objet {
-	private String nom;
+private String nom;
+private boolean typeObjetAlimentaire;
+private enum typeObjetEnum {Boisson,Nouritture,Arme,Vetements,Chaussures,Outils};
+private float prix;
+private int quantite;
+public String getNom() {
+	return nom;
+}
+public void setNom(String nom) {
+	this.nom = nom;
+}
+public boolean isTypeObjetAlimentaire() {
+	return typeObjetAlimentaire;
+}
+public void setTypeObjetAlimentaire(boolean typeObjetAlimentaire) {
+	this.typeObjetAlimentaire = typeObjetAlimentaire;
+}
+public float getPrix() {
+	return prix;
+}
+public void setPrix(float prix) {
+	this.prix = prix;
+}
+public int getQuantite() {
+	return quantite;
+}
+public void setQuantite(int quantite) {
+	this.quantite = quantite;
+}
 
-	public String getNom() {
-		return nom;
-	}
-
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-
-	private int quantite;
-
-	public int getQuantite() {
-		return quantite;
-	}
-
-	public void setQuantite(int quantite) {
-		this.quantite = quantite;
-	}
-	
-	
-	@Override
-	public String toString() {
-		return "objet : " +this.nom + "   quantité :  " + this.quantite;
-	}
 
 }
