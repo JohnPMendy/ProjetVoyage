8f3bb3881841586c701ef89f08f6a5d4ce9f2767
BadisG <110173477+BadisG@users.noreply.github.com>
2022-08-03 22:27:14 +0200

    Update Application.java

--- projetJeu/src/main/java/fr/projetjeu/Application.java
+++ projetJeu/src/main/java/fr/projetjeu/Application.java
@@ -1,17 +1,27 @@
 package fr.projetjeu;
 
+import java.awt.event.ActionEvent;
+
 import fr.projetjeu.model.Events;
 
 public class Application {
 
+	//static ActionEvent ae;
+	
 	public static void main(String[] args) {
+
 		Events events = new Events();
+		ActionEvent ae = new ActionEvent(args, 0, null);
 		events.setId(0);
 		events.getPerso().setAlive(true);
 
 		do {
-			events.affichageSituation();
-		} while (events.getPerso().isAlive()==true);
+			events.CreationInterface();
+			events.actionPerformed(ae);
+			events.testApplication();
+		} while (events.getPerso().isAlive() == true);
 
 	}
+
+
 }
