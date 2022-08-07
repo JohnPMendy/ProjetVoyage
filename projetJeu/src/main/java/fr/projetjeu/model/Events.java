ab5236519444a31ac632969e7e7cd335ae24ff5d
BadisG <110173477+BadisG@users.noreply.github.com>
2022-08-07 13:00:06 +0200

    Update Events.java

--- projetJeu/src/main/java/fr/projetjeu/model/Events.java
+++ projetJeu/src/main/java/fr/projetjeu/model/Events.java
@@ -1,21 +1,29 @@
 package fr.projetjeu.model;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import javax.swing.*;
-import javax.swing.event.*;
-import java.awt.event.*;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
-public class Events implements ActionListener {
+public class Events {
 
 	// Variables
 	private Scanner sc = new Scanner(System.in);
 	private static int id;
-	private int reponse;
-
+	private static int reponse;
+	private static int answer;
+	private static String histoire ="";
+	private static String r1 = "";
+	private static String r2 = "";
+	private static String r3 = "";
+	
 	// Liste des objets pour un evenement particulier
 	private ArrayList<Objet> listeObjetEvent = new ArrayList<>();
 	Personnage perso = new Personnage();
@@ -45,13 +53,6 @@ public class Events implements ActionListener {
 		this.id = id;
 	}
 
-	public int getReponse() {
-		return reponse;
-	}
-
-	public void setReponse(int reponse) {
-		this.reponse = reponse;
-	}
 
 	public ArrayList<Objet> getListeObjetEvent() {
 		return listeObjetEvent;
@@ -61,127 +62,233 @@ public class Events implements ActionListener {
 		this.listeObjetEvent = listeObjetEvent;
 	}
 
-	public static JButton btn = new JButton("1");
-	public static JButton btn2 = new JButton("2");
-	public static JButton btn3 = new JButton("3");
-	public static JButton btn4 = new JButton("Inventaire");
-	public static JButton btn5 = new JButton("Bouton Vide");
-	public static Events instance = new Events();
-	public static JFrame frame = new JFrame("Jeu : Le voyageur malchanceux");
-	public static JLabel label = new JLabel("Je suis un JLabel");
+	private static JDialog dialog;
+	private static JButton btn1 = new JButton("");
+	private static JButton btn2 = new JButton("");
+	private static JButton btn3 = new JButton("");
 
-	public void CreationInterface() {
-		btn.setBounds(100, 300, 100, 30);
-		btn2.setBounds(300, 300, 100, 30);
-		btn3.setBounds(500, 300, 100, 30);
-		btn4.setBounds(10, 10, 100, 30);
-		label.setBounds(300, 100, 100, 30);
+	public void TestApp() {
+		JFrame frame = new JFrame();
+		dialog = new JDialog(frame, "Jeu : Le voyageur malchanceux.", true);
+		dialog.setLayout(null);
+		JLabel label = new JLabel("Bienvenue, veuillez commencer la partie.", SwingConstants.CENTER);
+		JLabel labelID = new JLabel("Events: " + id);
+		JLabel labelAnswer = new JLabel("Answer: " + answer);
 
-		btn.addActionListener(instance);
-		btn2.addActionListener(instance);
-		frame.add(btn);
-		frame.add(btn2);
-		frame.add(btn3);
-		frame.add(btn4);
-		frame.add(label);
+		JButton startgame = new JButton("Commencer Partie");
 
-		frame.setSize(720, 480);
-		frame.setLayout(null);
-		frame.setVisible(true);
+		startgame.addActionListener(new ActionListener() {
 
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				histoire = "Question 0: Vous devez voyager, quel moyen de locomotion?";
+				r1 = "En avion";
+				r2 = "En bateau";
+				r3 = "À la nage";
+
+				testApplication();
+				label.setText(histoire);
+				btn1.setText(r1);
+				btn2.setText(r2);
+				btn3.setText(r3);
+				id = 0;
+				answer = 0;
+				labelID.setText("Events: " + id);
+				labelAnswer.setText("Answer: " + answer);
+				rd(3);
+			}
+
+		});
+
+		btn1.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				reponse = 1;
+				testApplication();
+				labelID.setText("Events: " + id);
+				labelAnswer.setText("Answer: " + answer);
+				label.setText(histoire);
+				btn1.setText(r1);
+				btn2.setText(r2);
+				btn3.setText(r3);
+
+			}
+		});
+
+		btn2.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				reponse = 2;
+				testApplication();
+				labelID.setText("Events: " + id);
+				labelAnswer.setText("Answer: " + answer);
+				label.setText(histoire);
+				btn1.setText(r1);
+				btn2.setText(r2);
+				btn3.setText(r3);
+
+			}
+		});
+
+		btn3.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				reponse = 3;
+				testApplication();
+				labelID.setText("Events: " + id);
+				labelAnswer.setText("Answer: " + answer);
+				label.setText(histoire);
+				btn1.setText(r1);
+				btn2.setText(r2);
+				btn3.setText(r3);
+
+			}
+		});
+
+		dialog.add(label);
+		dialog.add(labelID);
+		dialog.add(labelAnswer);
+		dialog.add(btn1);
+		dialog.add(btn2);
+		dialog.add(btn3);
+		btn1.setBounds(70, 350, 150, 50); // x , y , taille x , taille y
+		btn2.setBounds(270, 350, 150, 50);
+		btn3.setBounds(470, 350, 150, 50);
+		startgame.setBounds(500, 30, 200, 30);
+		label.setBounds(100, 100, 500, 200);
+		labelID.setBounds(10, 10, 150, 30);
+		labelAnswer.setBounds(100, 10, 150, 30);
+		// label.setForeground(new Color(100, 100, 100)); //Couleur du texte
+		label.setBackground(new Color(200, 200, 200));
+		label.setOpaque(true);
+		btn1.setVisible(false);
+		btn2.setVisible(false);
+		btn3.setVisible(false);
+		dialog.add(startgame);
+		dialog.setSize(720, 480);
+		dialog.setVisible(true);
 	}
 
-	public void actionPerformed(ActionEvent e) {
-		if (e.getSource().equals(btn)) {
-			frame.setVisible(false);
+	public void rd(int i) { // RD = Réponses Disponibles
+
+		if (i == 0) {
+			btn1.setVisible(false);
+			btn2.setVisible(false);
+			btn3.setVisible(false);
 		}
+
+		else if (i == 1) {
+			btn1.setVisible(true);
+			btn1.setBounds(130, 350, 450, 50);
+			btn2.setVisible(false);
+			btn3.setVisible(false);
+		}
+
+		else if (i == 2) {
+			btn1.setVisible(true);
+			btn1.setBounds(130, 350, 200, 50);
+			btn2.setVisible(true);
+			btn2.setBounds(370, 350, 200, 50);
+			btn3.setVisible(false);
+		}
+
+		else {
+			btn1.setVisible(true);
+			btn2.setVisible(true);
+			btn3.setVisible(true);
+			
+			btn1.setBounds(70, 350, 150, 50); 
+			btn2.setBounds(270, 350, 150, 50);
+			btn3.setBounds(470, 350, 150, 50);
+		}
+	}
+
+	public void situation(int i) {
+		id = id + 1;
+		answer = i;
 	}
 
 	public void testApplication() {
 
-		if (id == 0)
-		// Exemple,
+		if (id == 0) 
+
 		{
-			System.out.println("Question " + id + ": Vous devez voyager, quel moyen de locomotion?");
-			System.out.println("Reponse 1: Avion");
-			System.out.println("Reponse 2: Bateau");
-			System.out.println("Reponse 3: A la nage");
-			System.out.println("Inserez votre réponse (1,2 ou 3) : ");
-			reponse = sc.nextInt();
+			
+			if (reponse == 1) {
+				situation(1);
+				rd(2);
 
-			if (reponse == 1 || reponse == 2) {
-				id = id + 1;
+				histoire = "Question 1: Durant le trajet en avion, ouvrir le cockpit?";
+				r1 = "Oui";
+				r2 = "Non";
+				r3 = "";
 			}
 
-			else if (reponse == 3) {
-				System.out.println("Vous êtes mort");
+			else if (reponse == 2) {
+				situation(2);
+				rd(2);
+				
+				histoire = "Question 1: Durant le trajet en bateau, sauter par dessus bord?";
+				r1 = "Oui";
+				r2 = "Non";
+				r3 = "";
+			}
 
+			else if (reponse == 3) { 
+				situation(3);
+				rd(0);
 				perso.setAlive(false);
-
-			}
-
-			else {
-				System.out.println("Vous n'avez pas saisi une valeur adequate");
+				
+				histoire = "Vous êtes mort.";
 
 			}
 
 		}
 
-		else if (id == 1 && reponse == 1) {
-			System.out.println("Question " + id + ": Durant le trajet en avion, ouvrir le cockpit?");
-			System.out.println("Reponse 1: Oui");
-			System.out.println("Reponse 2: Non");
-			System.out.println("Inserez votre réponse (1 ou 2) : ");
-			reponse = sc.nextInt();
+		else if (id == 1 && answer == 1) {
+
+			
+			if (reponse == 1) {
+				situation(1);
+				rd(0);
+				histoire = "Vous êtes mort.";
+				perso.setAlive(false);
+
+			}
+
+			else if (reponse == 2) {
+				situation(2);
+				rd(0);
+				histoire = "A suivre...";
+
+			}
+
+		}
+
+		else if (id == 1 && answer == 2) {
 
 			if (reponse == 1) {
-				System.out.println("Vous êtes mort");
-
+				situation(1);
+				rd(0);
+				histoire = "Vous êtes mort.";
 				perso.setAlive(false);
 			}
 
 			else if (reponse == 2) {
-				id = id + 1;
+				situation(2);
+				rd(0);
+				histoire = "A suivre...";
 			}
 
-			else {
-				System.out.println("Vous n'avez pas saisi une valeur adequate");
-
-			}
-
-		}
-
-		else if (id == 1 && reponse == 2) {
-			System.out.println("Question " + id + ": Durant le trajet en bateau, sauter par dessus bord?");
-			System.out.println("Reponse 1: Oui");
-			System.out.println("Reponse 2: Non");
-			System.out.println("Inserez votre réponse (1 ou 2) : ");
-			reponse = sc.nextInt();
-
-			if (reponse == 1) {
-				System.out.println("Vous êtes mort");
-				perso.setAlive(false);
-			}
-
-			else if (reponse == 2) {
-				id = id + 1;
-			}
-
-			else {
-				System.out.println("Vous n'avez pas saisi une valeur adequate");
-
-			}
-
-		}
-		
-
-		else {
-			System.out.println("D'autres questions à venir... FIN");
-			perso.setAlive(false);
-
+			
 		}
 
 	}
-
 
 }
