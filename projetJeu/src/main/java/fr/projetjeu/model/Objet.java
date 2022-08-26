932a8a953da0bcee1a2c8a34ef8087b518e9f889
MAHMOUDMOUSSAOUI98 <mahmoudmoussaoui8@gmail.com>
2022-08-26 12:06:54 +0200

    ADDING obj.java

--- projetJeu/src/main/java/fr/projetjeu/model/Objet.java
+++ projetJeu/src/main/java/fr/projetjeu/model/Objet.java
@@ -4,9 +4,13 @@ public class Objet {
 private int id;
 private String nom;
 private boolean typeObjetAlimentaire;
-private TypeObjets typeObjets;
 private float prix;
-private int quantite;
+private TypeObjets typeObjets;
+private int quantiteInventaire;
+private int quantiteBoutique;
+private Boutique boutique;
+private Inventaire inventaire;
+
 public String getNom() {
 	return nom;
 }
@@ -38,13 +42,41 @@ public void setTypeObjets(TypeObjets typeObjets) {
 public void setPrix(float prix) {
 	this.prix = prix;
 }
-public int getQuantite() {
-	return quantite;
+
+
+public int getQuantiteInventaire() {
+	return quantiteInventaire;
 }
-public void setQuantite(int quantite) {
-	this.quantite = quantite;
+public void setQuantiteInventaire(int quantiteInventaire) {
+	this.quantiteInventaire = quantiteInventaire;
 }
+
+
+public int getQuantiteBoutique() {
+	return quantiteBoutique;
+}
+public void setQuantiteBoutique(int quantiteBoutique) {
+	this.quantiteBoutique = quantiteBoutique;
+}
+
+
+public Boutique getBoutique() {
+	return boutique;
+}
+public void setBoutique(Boutique boutique) {
+	this.boutique = boutique;
+}
+
+
+public Inventaire getInventaire() {
+	return inventaire;
+}
+public void setInventaire(Inventaire inventaire) {
+	this.inventaire = inventaire;
+}
+
+
 public String toString() {
-	return this.nom +"--Prix="+ this.prix+"--Qte="+this.quantite ;
+	return this.nom +"--Prix="+ this.prix+"--Qte dans boutique="+this.quantiteBoutique +"--Qte votre inventaire="+this.quantiteInventaire;
 }
 }
