96d673d5ef9711fc58ab4aa935da51b8088b38e9
Clotilde <clotilde.zenone@isen.yncrea.fr>
2022-09-05 14:52:16 +0200

    Repo Reponse (from badis)

---
+++ projetJeuMaven/src/main/java/fr/projetjeu/repo/jpa/ReponseRepositoryJpa.java
@@ -0,0 +1,48 @@
+package fr.projetjeu.repo.jpa;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import fr.projetjeu.exception.CantDeleteException;
+import fr.projetjeu.model.Events;
+import fr.projetjeu.model.Reponse;
+import fr.projetjeu.repo.IEventRepository;
+import fr.projetjeu.repo.IReponseRepository;
+
+public class ReponseRepositoryJpa extends AbstractRepositoryJpa<Reponse> implements IReponseRepository {
+
+	public ReponseRepositoryJpa() {
+		super(Reponse.class);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public List<Reponse> findByEvenementId(int id) {
+		// TODO Auto-generated method stub
+		List<Reponse> reponses = new ArrayList<>();
+		EntityManager em = emf.createEntityManager();
+
+		try {
+
+			em.getTransaction().begin();
+
+			em.createQuery("SELECT * FROM reponse WHERE rep_evenement_id = ?1").setParameter(1, id).executeUpdate();
+
+			em.getTransaction().commit();
+
+		}
+
+		catch (Exception e) {
+			return null;
+		} finally {
+			em.close();
+		}
+		return reponses;
+	}
+
+}
