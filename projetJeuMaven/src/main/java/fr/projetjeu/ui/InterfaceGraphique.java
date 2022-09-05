bb5042fb791d354a9f8767eec368740220322de3
BadisG <110173477+BadisG@users.noreply.github.com>
2022-09-05 16:46:06 +0200

    Update InterfaceGraphique.java

--- projetJeuMaven/src/main/java/fr/projetjeu/ui/InterfaceGraphique.java
+++ projetJeuMaven/src/main/java/fr/projetjeu/ui/InterfaceGraphique.java
@@ -3,6 +3,7 @@ package fr.projetjeu.ui;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -12,9 +13,24 @@ import javax.swing.SwingConstants;
 
 import fr.projetjeu.exception.NegativeIdException;
 import fr.projetjeu.exception.ReponseNotFoundException;
+import fr.projetjeu.model.Events;
+import fr.projetjeu.model.Inventaire;
+import fr.projetjeu.model.Reponse;
+import fr.projetjeu.service.EventsService;
+import fr.projetjeu.service.ReponseService;
 
 public class InterfaceGraphique {
-
+	
+	EventsService es = new EventsService();
+	Inventaire i = new Inventaire();
+	ReponseService rs = new ReponseService();
+	Events events = new Events();
+	
+	private int numReponse;
+	int id = events.getId();
+	String histoire = events.getHistoire();
+	List<Reponse> reponse = events.getReponses();
+	
 	private static JDialog dialog;
 	private static JButton btn1 = new JButton(""); // Réponse 1
 	private static JButton btn2 = new JButton(""); // Réponse 2
@@ -26,7 +42,7 @@ public class InterfaceGraphique {
 		dialog = new JDialog(frame, "Jeu : Le voyageur malchanceux.", true);
 		dialog.setLayout(null);
 		JLabel label = new JLabel("Bienvenue, veuillez commencer la partie.", SwingConstants.CENTER);
-		JLabel labelID = new JLabel("id " + id);
+		JLabel labelID = new JLabel("id " + events.getId());
 
 		JButton startgame = new JButton("Commencer Partie");
 
@@ -38,19 +54,22 @@ public class InterfaceGraphique {
 				histoire = es.findById(id).getHistoire();
 
 				try {
-					reponse.add(0, rs.findByEvenementId(id).get(0).getTexte());
-					reponse.add(1, rs.findByEvenementId(id).get(1).getTexte());
-					reponse.add(2, rs.findByEvenementId(id).get(2).getTexte());
+					reponse.add(0, rs.findByEvenementId(id).get(0));
+					reponse.add(1, rs.findByEvenementId(id).get(1));
+					reponse.add(2, rs.findByEvenementId(id).get(2));
+					//reponse.add(0, rs.findByEvenementId(id).get(0).getTexte());
+					//reponse.add(1, rs.findByEvenementId(id).get(1).getTexte());
+					//reponse.add(2, rs.findByEvenementId(id).get(2).getTexte());
 
 				} catch (NegativeIdException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
 				// testApplication();
-				label.setText(histoire);
-				btn1.setText((String) reponse.get(0));
-				btn2.setText((String) reponse.get(1));
-				btn3.setText((String) reponse.get(2));
+				label.setText(events.getHistoire());
+				btn1.setText(reponse.get(0).getTexte());
+				btn2.setText(reponse.get(1).getTexte());
+				btn3.setText(reponse.get(2).getTexte());
 				labelID.setText("id " + id);
 				rd(3);
 				btninventaire.setVisible(true);
@@ -152,7 +171,8 @@ public class InterfaceGraphique {
 
 			// Recup de la réponse depuis la BDD
 			try {
-				reponse.add(0, rs.findByEvenementId(id).get(0).getTexte());
+				reponse.add(0, rs.findByEvenementId(id).get(0));
+				
 
 			} catch (NegativeIdException e1) {
 				// TODO Auto-generated catch block
@@ -160,7 +180,7 @@ public class InterfaceGraphique {
 			}
 
 			// Ajout de la réponse dans un bouton
-			btn1.setText((String) reponse.get(0));
+			btn1.setText(reponse.get(0).getTexte());
 
 			// Recup de l'histoire depuis la BDD
 			histoire = es.findById(id).getHistoire();
@@ -174,16 +194,16 @@ public class InterfaceGraphique {
 			btn3.setVisible(false);
 
 			try {
-				reponse.add(0, rs.findByEvenementId(id).get(0).getTexte());
-				reponse.add(1, rs.findByEvenementId(id).get(1).getTexte());
+				reponse.add(0, rs.findByEvenementId(id).get(0));
+				reponse.add(1, rs.findByEvenementId(id).get(1));
 
 			} catch (NegativeIdException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
 
-			btn1.setText((String) reponse.get(0));
-			btn2.setText((String) reponse.get(1));
+			btn1.setText(reponse.get(0).getTexte());
+			btn2.setText(reponse.get(1).getTexte());
 
 			histoire = es.findById(id).getHistoire();
 
@@ -199,18 +219,18 @@ public class InterfaceGraphique {
 			btn3.setBounds(470, 350, 150, 50);
 
 			try {
-				reponse.add(0, rs.findByEvenementId(id).get(0).getTexte());
-				reponse.add(1, rs.findByEvenementId(id).get(1).getTexte());
-				reponse.add(2, rs.findByEvenementId(id).get(2).getTexte());
+				reponse.add(0, rs.findByEvenementId(id).get(0));
+				reponse.add(1, rs.findByEvenementId(id).get(1));
+				reponse.add(2, rs.findByEvenementId(id).get(2));
 
 			} catch (NegativeIdException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
 
-			btn1.setText((String) reponse.get(0));
-			btn2.setText((String) reponse.get(1));
-			btn3.setText((String) reponse.get(2));
+			btn1.setText(reponse.get(0).getTexte());
+			btn2.setText(reponse.get(1).getTexte());
+			btn1.setText(reponse.get(2).getTexte());
 
 			histoire = es.findById(id).getHistoire();
 
@@ -220,7 +240,7 @@ public class InterfaceGraphique {
 	public void testApplication() {
 
 		try {
-			id = rs.findByEvenementId(id).get(numReponse - 1).getProchainEvenementId();
+			id = rs.findByEvenementId(id).get(numReponse - 1).getProchainEvenementId().getId();
 		} catch (NegativeIdException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
