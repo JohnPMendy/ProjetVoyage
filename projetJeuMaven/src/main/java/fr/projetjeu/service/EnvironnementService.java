bf805b5b51c4693a397e2bd9aa26878e569f6eb8
John Mendy <dymenjohn@gmail.com>
2022-09-05 12:17:16 +0200

    mise à jour

--- projetJeuMaven/src/main/java/fr/projetjeu/service/EnvironnementService.java
+++ projetJeuMaven/src/main/java/fr/projetjeu/service/EnvironnementService.java
@@ -1,25 +1,62 @@
 package fr.projetjeu.service;
 
-import fr.projetjeu.exception.EnvironnementNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.projetjeu.exception.EntityNotFoundException;
 import fr.projetjeu.exception.InvalidArgsException;
+import fr.projetjeu.exception.InvalidEntityException;
 import fr.projetjeu.model.Environnement;
 import fr.projetjeu.repo.IEnvironnementRepository;
-import fr.projetjeu.repo.sql.EnvironnementRepositorySql;
+import fr.projetjeu.repo.jpa.EnvironnementRepositoryJpa;
 
 public class EnvironnementService {
-	private IEnvironnementRepository repoEnvironnemnt = new EnvironnementRepositorySql();
+	private IEnvironnementRepository repoEnvironnement = new EnvironnementRepositoryJpa();
 	
 	
 	public Environnement findById(int id) {
 		if (id<=0) {
-			throw new InvalidArgsException();
+			throw new InvalidArgsException("id");
 		}
-		Environnement env = this.repoEnvironnemnt.findById(id);
+		Environnement env = this.repoEnvironnement.findById(id);
 		
 		if(env==null) {
-			throw new EnvironnementNotFoundException();
+			throw new EntityNotFoundException();
 		}
 		return env;
 		
 	}
+	
+	public List<Environnement> findAll() {
+		List<Environnement> environnements = repoEnvironnement.findAll();
+		
+		if (environnements == null) {
+			return new ArrayList<>();
+		}
+		
+		return environnements;
+	}
+	
+	
+	public void save(Environnement environnement) {
+		if (environnement.getNom() == null || environnement.getNom().isBlank()) {
+			throw new InvalidEntityException("nom");
+		}
+		
+		if (environnement.getMeteo() == null || environnement.getEnvironnement()==null) {
+			throw new InvalidEntityException("responsable");
+		}
+		
+		repoEnvironnement.save(environnement);
+	}
+	
+	public void deleteById(int id) {
+		if (id<=0) {
+			throw new InvalidArgsException("id");
+		}
+		
+		repoEnvironnement.deleteById(id);
+	}
+	
+	
 }
