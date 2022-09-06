350f4d0e618495fcd5596000b4c2b35739c902e9
John Mendy <dymenjohn@gmail.com>
2022-09-06 11:00:15 +0200

    mise à jour des classes

--- projetJeuMaven/src/main/java/fr/projetjeu/model/Boutique.java
+++ projetJeuMaven/src/main/java/fr/projetjeu/model/Boutique.java
@@ -1,8 +1,6 @@
 package fr.projetjeu.model;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -14,30 +12,25 @@ import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
-
 @Entity
-@Table(name="boutique")
+@Table(name = "boutique")
 public class Boutique {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "btq_id")
 	private int id;
-	
-	@Column(name="btq_nom", length=100)
-	private String nom;
-	
-	@Enumerated(EnumType.ORDINAL)
-	@Column(name="btq_type",nullable=false)
-	private TypeBoutique typeBoutique;
-	
-	@ManyToMany(mappedBy="boutiques")
-	List<Objet> objets;
-	
-	
-	static Scanner sc = new Scanner(System.in);
 
-		
+	@Column(name = "btq_nom", length = 100)
+	private String nom;
+
+	@Enumerated(EnumType.ORDINAL)
+	@Column(name = "btq_type", nullable = false)
+	private TypeBoutique typeBoutique;
+
+	@ManyToMany(mappedBy = "boutiques")
+	List<Objet> objets;
+
 	public int getId() {
 		return id;
 	}
@@ -54,14 +47,6 @@ public class Boutique {
 		this.objets = objets;
 	}
 
-	public Scanner getSc() {
-		return sc;
-	}
-
-	public void setSc(Scanner sc) {
-		Boutique.sc = sc;
-	}
-
 	public String getNom() {
 		return nom;
 	}
@@ -76,80 +61,6 @@ public class Boutique {
 
 	public void setNom(String nom) {
 		this.nom = nom;
-	}
-
-	/*
-	 * public void achatObjets(Events e, Inventaire i, int idx,int qteAchete) { //
-	 * idx c'est l'indice de l'objet a acheter de la boutique
-	 * i.getListeObjetInventaire().add(e.getListeObjetEvent().get(idx)); }
-	 * 
-	 * public void venteObjets(Inventaire i, int idx) { // idx c'est l'indice de
-	 * l'objet a vendre a la boutique i.getListeObjetInventaire().remove(idx);
-	 * 
-	 * }
-	 */
-// ca affiche precisement les objets de l'evenement :pas utilisee !!!!
-	public void AffichageObjetsBoutique(Events e) {
-		List<Objet> listeObjBoutique = new ArrayList<>();
-		listeObjBoutique = e.getListeObjetEvent();
-		int j = 1;
-		for (Objet obj : listeObjBoutique) {
-			System.out.println(j + "--" + obj);
-			j++;
-		}
-	}
-
-//ca affiche toutes les listes de type objets: utilisee !!
-	public void AffichageObjets(List<Objet> A) {
-		int j = 1;
-		for (Objet obj : A) {
-			System.out.println(j + "--" + obj);
-			j++;
-		}
-	}
-
-	public void achatObjet(Events e, Inventaire i, Personnage p) {
-		// le type de la boutique depend de l'evenement
-		System.out.println("vous etes dans " + this.typeBoutique);
-		System.out.println("Les articles disponibles dans cette boutique sont :");
-		// AffichageObjetsBoutique(e);
-		AffichageObjets(e.getListeObjetEvent());
-		System.out
-				.println("Si vous etes interesse par un ou plusieurs articles de cette boutique tapez 1 sinon tapez 0");
-		if (sc.nextInt() == 0)
-			return;
-		else
-			System.out.println("quel est l'article que vous avez choisi ? ");
-		System.out.println("N.B : Chaqu'un de nos articles correspond a un indice en chiffres 1,2,... ");
-		int idx = sc.nextInt();
-		System.out.println("quelle est la quantite que vous voulez ?");
-		int qteAchete = sc.nextInt();
-		Objet obj = e.getListeObjetEvent().get(idx - 1);
-		float prix = obj.getPrix() * qteAchete;
-		if (p.getArgent() >= prix) // verification si le personnage a assez de sous pour acheter les produits
-									// selectionne
-		{
-			p.setArgent(p.getArgent() - prix);
-			i.ajoutObjet(obj, qteAchete);
-		} else {
-			System.out.println("vous n'avez pas assez de sous pour acheter ces produits");
-			return;
-		}
-	}
-
-	public void venteObjet(Events e, Inventaire i, Personnage p) {
-		System.out.println("vous etes dans " + this.typeBoutique);
-		System.out.println("les articles que vous disposez sont ");
-		AffichageObjets(i.getListeObjetInventaire());
-		System.out.println("Quel article voulez vous vendre ?"); // a voir est ce que la boutique achete ou pas les
-																	// articles
-		int idx = sc.nextInt();
-		System.out.println("quelle est la quantite que vous voulez vendre ?");
-		int qteVendue = sc.nextInt();
-		Objet obj = i.getListeObjetInventaire().get(idx - 1);
-		float prix = obj.getPrix() * qteVendue;
-		p.setArgent(p.getArgent() + prix);
-		i.supprimerObjet(obj, qteVendue);
 	}
 
 }
--- projetJeuMaven/src/main/java/fr/projetjeu/model/Inventaire.java
+++ projetJeuMaven/src/main/java/fr/projetjeu/model/Inventaire.java
@@ -12,22 +12,21 @@ import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
-
 @Entity
-@Table(name="inventaire")
+@Table(name = "inventaire")
 public class Inventaire {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "inv_id")
 	private int id;
-	
+
 	@ManyToMany(mappedBy = "inventaires")
 	private List<Objet> listeObjetInventaire = new ArrayList<>();
-	
+
 	@OneToOne(mappedBy = "inventaire")
 	private Partie partie;
-	
+
 	public int getId() {
 		return id;
 	}
@@ -35,8 +34,6 @@ public class Inventaire {
 	public void setId(int id) {
 		this.id = id;
 	}
-	
-	
 
 	public Partie getPartie() {
 		return partie;
@@ -47,58 +44,10 @@ public class Inventaire {
 	}
 
 	public List<Objet> getListeObjetInventaire() {
-		System.out.println("Voici la liste des objets disponible dans votre inventaire :");
-		afficherListObjet();
 		return listeObjetInventaire;
 	}
 
 	public void setListeObjetInventaire(List<Objet> listeObjetInventaire) {
 		this.listeObjetInventaire = listeObjetInventaire;
 	}
-
-	public void ajoutObjet(Objet obj, int quantite) {
-		// nﾃｩcessaire de vﾃｩrifier si l'objet est dﾃｩjﾃ� prﾃｩsent dans l'invetnaire, si
-		// c'est le cas
-		// on augmente juste la quantitﾃｩ prﾃｩsente sinon on crﾃｩe un objet
-		if (this.listeObjetInventaire.contains(obj)) {
-			int index = listeObjetInventaire.indexOf(obj);
-			Objet objet = listeObjetInventaire.get(index);
-			objet.setQuantiteInventaire(objet.getQuantiteInventaire() + quantite);
-		} else {
-			obj.setQuantiteInventaire(quantite);
-			this.listeObjetInventaire.add(obj);
-		}
-		System.out.println(obj.getQuantiteInventaire() + " " + obj.getNom() + " ajoutﾃｩ ﾃ� l'inventaire ! ");
-	}
-
-	public void supprimerObjet(Objet obj, int quantite) {
-		if (this.listeObjetInventaire.contains(obj)) { // vﾃｩrifier si l'objet existe dans l'inventaire
-			int index = listeObjetInventaire.indexOf(obj);
-			Objet objet = listeObjetInventaire.get(index);
-			if (quantite == objet.getQuantiteInventaire()) { // si la quantitﾃｩ qu'on veut supprimer est ﾃｩgale a la quantitﾃｩ totale
-													// de chose qu'on veut supprimer, on supprime l'objet
-				this.listeObjetInventaire.remove(obj);
-				System.out.println(obj.getNom() + " a ﾃｩtﾃｩ supprimﾃｩ de l'inventaire !");
-			} else if (quantite < objet.getQuantiteInventaire() && quantite > 0) {
-				obj.setQuantiteInventaire(obj.getQuantiteInventaire() - quantite);
-				if (quantite == 1) {
-					System.out.println("un exemplaire de " + obj.getNom() + " a ﾃｩtﾃｩ supprimﾃｩ de l'inventaire");
-				}
-				System.out.println(quantite + " exemplaires de " + obj.getNom() + " ont ﾃｩtﾃｩ supprimﾃｩs de l'inventaire");
-			} else {
-				System.out.println("Mauvaise quantite selectionnﾃｩ");
-			}
-
-		} else {
-			System.out.println("Cet objet n'existe pas dans l'inventaire...");
-		}
-	}
-
-	/// petite fonction permettant l'affichage de l'inventaire integral
-	public void afficherListObjet() {
-		for (Objet objet : this.listeObjetInventaire) {
-			System.out.println("objet : " + objet.getNom() + " ,quantitﾃｩ : " + objet.getQuantiteInventaire()+" ,type d'objet : "+ objet.getTypeObjets()); //regarder comment recuperer le type d'objet depuis l'Enum
-		}
-	}
-
 }
