a2bc1d5c1855cc8018ae22ce0ffc8de11f733153
John Mendy <dymenjohn@gmail.com>
2022-09-26 10:58:19 +0200

    mise à jour de la popup chargement dans jeu interactif

--- HTML-CSS-JS/assets/js/JeuInteractif.js
+++ HTML-CSS-JS/assets/js/JeuInteractif.js
@@ -1,53 +1,55 @@
+//ouvre la selection de la partie à charger
 document
 .querySelector('input.Charger')
 .addEventListener('click', () => {
     document
-    .querySelector('.popup-overlay')
-    // .style.display = 'block';
+    .querySelector('.popup-overlay-chargement')
     .classList.add('open');
 
     document
-    .querySelector('.popup-container')
+    .querySelector('.popup-container-chargement')
     .classList.add('open');
 });
 
+//chargment de la partie selectionné avec ouvertue d'une popup de chargement reussi
 const listeBoutons = document.querySelectorAll('button.ligneChargement');
 listeBoutons.forEach(bouton=>{
     bouton.addEventListener('click',()=>{
 
         document
-        .querySelector('.popup-container')
+        .querySelector('.popup-container-chargement')
         .classList.remove('open');
     
         document
-        .querySelector('.popup-container-chargement')
+        .querySelector('.popup-charge-confirme')
         .classList.add('open');
     
     });
 });
 
 
-// Fermeture de la popup
+// Fermeture de la popup et retour à la partie
 document
-.querySelector('button.close-popup')
+.querySelector('button.close-popup-chargement')
 .addEventListener('click', () => {
     document
-    .querySelector('.popup-overlay')
-    .classList.remove('open');
-
-    document
-    .querySelector('.popup-container')
-    .classList.remove('open');
-});
-
-document
-.querySelector('button.close-chargmement')
-.addEventListener('click', () => {
-    document
-    .querySelector('.popup-overlay')
+    .querySelector('.popup-overlay-chargement')
     .classList.remove('open');
 
     document
     .querySelector('.popup-container-chargement')
+    .classList.remove('open');
+});
+
+//Fermeture de la validation de chagement et retour à la partie
+document
+.querySelector('button.close-charge-confirme')
+.addEventListener('click', () => {
+    document
+    .querySelector('.popup-overlay-chargement')
+    .classList.remove('open');
+
+    document
+    .querySelector('.popup-charge-confirme')
     .classList.remove('open');
 });
