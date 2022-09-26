5075665a935a8aa349bba270ce335566de63596f
BadisG <110173477+BadisG@users.noreply.github.com>
2022-09-26 15:23:33 +0200

    Update JsonViews.java

--- projetJeuMaven/src/main/java/fr/projetjeu/model/JsonViews.java
+++ projetJeuMaven/src/main/java/fr/projetjeu/model/JsonViews.java
@@ -1,10 +1,30 @@
-package fr.projetjeu.model;
+package fr.formation.model;
 
 public class JsonViews {
 
+	//classe imbriquee pour ne pas avoir plein de fichier avec juste 
+	//class XXX{}
+	//projection pour tout les types simples qui ne posent pas de probleme
 	public static class Common{
 		
 	}
-
+	//view pour produit 
+	public static class Events extends Common{
+		
+	}
 	
+	//view pour produit avec fournisseur
+	public static class EventsAvecReponses extends Events{
+		
+	}
+	
+	public static class Fournisseur extends Common{
+		
+	}
+	
+	public static class FournisseurAvecProduit extends Fournisseur{
+		
+	}
 }
+
+
