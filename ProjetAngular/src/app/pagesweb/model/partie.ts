c24354e1f975a9ddd73188bfa5c4555095ada0e5
John Mendy <dymenjohn@gmail.com>
2022-10-07 16:46:29 +0200

    partie.ts

--- ProjetAngular/src/app/pagesweb/model/partie.ts
+++ ProjetAngular/src/app/pagesweb/model/partie.ts
@@ -1,3 +1,9 @@
 export class Partie {
+  public get id(): number | undefined {
+    return this._id;
+  }
+  public set id(value: number | undefined) {
+    this._id = value;
+  }
   constructor(private _id?: number) {}
 }
