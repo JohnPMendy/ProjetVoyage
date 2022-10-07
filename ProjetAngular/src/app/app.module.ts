4bbb153dc5dd316f85437b7d8d3687c1d648f1dd
MAHMOUDMOUSSAOUI98 <mahmoudmoussaoui8@gmail.com>
2022-10-07 15:30:00 +0200

    securitee et formulaire de connection

--- ProjetAngular/src/app/app.module.ts
+++ ProjetAngular/src/app/app.module.ts
@@ -1,7 +1,6 @@
+import { AuthenticationInterceptor } from './pagesweb/interceptor/authentification.interceptor';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { routes } from './routes';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
@@ -9,6 +8,8 @@ import { JeuComponent } from './pagesweb/component/jeu/jeu.component';
 import { AccueilComponent } from './pagesweb/component/accueil/accueil.component';
 import { ConnectionComponent } from './pagesweb/component/connection/connection.component';
 import { PartieComponent } from './pagesweb/component/partie/partie.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent, JeuComponent, AccueilComponent, ConnectionComponent, PartieComponent],
@@ -17,8 +18,9 @@ import { PartieComponent } from './pagesweb/component/partie/partie.component';
     FormsModule,
     RouterModule.forRoot(routes),
     HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [{provide:HTTP_INTERCEPTORS,useClass: AuthenticationInterceptor ,multi:true}],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- ProjetAngular/src/app/pagesweb/component/connection/connection.component.ts
+++ ProjetAngular/src/app/pagesweb/component/connection/connection.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from '../../service/auth.service';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  AbstractControl,
+  ValidationErrors,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-connection',
   templateUrl: './connection.component.html',
@@ -7,9 +15,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConnectionComponent implements OnInit {
 
-  constructor() { }
+  login: string = '';
+  password: string = '';
+  form!: FormGroup;
+
+  constructor(private authService: AuthService ,private router: Router) {}
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      inputCtrl: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+
+      ]),
+      infoGrp: new FormGroup(
+        {
+          loginCtrl: new FormControl('', Validators.required),
+          passwordCtrl: new FormControl('', Validators.required)
+        },
+      ),
+    });
   }
 
+  submit() {
+    this.authService.authentication(this.login, this.password).subscribe({
+      next: (data) => {
+        console.log(data);
+        sessionStorage.setItem('compte', JSON.stringify(data));
+        sessionStorage.setItem(
+          'token',
+          'Basic ' + btoa(this.login + ':' + this.password)
+        );
+        this.router.navigateByUrl('/partie')
+      }, //succes
+      error: (err) => {
+        console.log(err);
+      }, //erreur
+    });
+  }
+
+  // loginAndPrenomNotEquals(control: AbstractControl): ValidationErrors | null {
+  //   let group = control as FormGroup;
+  //   let prenom = group.controls['prenomCtrl'].value;
+  //   let login = group.get('loginCtrl')?.value;
+  //   return prenom == login ? { loginPrenomEquals: true } : null;
+  // }
+
 }
+
+
+
+
+
+
+
+
+
