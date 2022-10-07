a59ad86a8b259eb1f46edccb5851560cde20daef
John Mendy <dymenjohn@gmail.com>
2022-10-07 17:15:48 +0200

    création model et service de partie et environnement angulat

--- ProjetAngular/src/app/pagesweb/service/partie.service.ts
+++ ProjetAngular/src/app/pagesweb/service/partie.service.ts
@@ -1,3 +1,5 @@
+import { Partie } from './../../pagesweb/model/partie';
+import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
@@ -8,4 +10,12 @@ export class PartieService {
   private static URL = 'http://localhost:8080/voyages/api/partie';
 
   constructor(private httpClient: HttpClient) {}
+
+  public findById(id: number): Observable<Partie> {
+    return this.httpClient.get<Partie>(PartieService.URL + '/' + id);
+  }
+
+  public delete(id: number): Observable<void> {
+    return this.httpClient.delete<void>(PartieService + '/' + id);
+  }
 }
