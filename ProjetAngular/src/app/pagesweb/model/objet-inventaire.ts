421664addcbee6a02303889556a7d6c82fc5f16d
BadisG <110173477+BadisG@users.noreply.github.com>
2022-10-11 12:13:08 +0200

    correction typo Inventaire

--- ProjetAngular/src/app/pagesweb/model/objet-inventaire.ts
+++ ProjetAngular/src/app/pagesweb/model/objet-inventaire.ts
@@ -1,34 +1,29 @@
-import { Inventaire } from "./inventaire";
+import { Inventaire } from './inventaire';
 
 export class ObjetInventaire {
-
-  public get quantiteinventaire(): number|undefined  {
+  public get quantiteinventaire(): number | undefined {
     return this._quantiteinventaire;
   }
-  public set quantiteinventaire(value: number|undefined ) {
+  public set quantiteinventaire(value: number | undefined) {
     this._quantiteinventaire = value;
   }
-  public get objet(): Object |undefined {
+  public get objet(): Object | undefined {
     return this._objet;
   }
-  public set objet(value: Object|undefined ) {
+  public set objet(value: Object | undefined) {
     this._objet = value;
   }
-  public get inventaire(): Inventaire|undefined  {
+  public get inventaire(): Inventaire | undefined {
     return this._inventaire;
   }
-  public set inventaire(value: Inventaire|undefined ) {
+  public set inventaire(value: Inventaire | undefined) {
     this._inventaire = value;
   }
 
-
-
   constructor(
     private _id?: number,
-    private _inventaire?: inventaire,
+    private _inventaire?: Inventaire,
     private _objet?: Object,
     private _quantiteinventaire?: number
-    ){}
+  ) {}
 }
-
-
