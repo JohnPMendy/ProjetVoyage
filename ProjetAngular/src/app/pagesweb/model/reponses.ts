ec82c316a505d14be3b7e64fd4322f46b7f8eec0
BadisG <110173477+BadisG@users.noreply.github.com>
2022-10-11 15:30:53 +0200

    jeu fonctionel

--- ProjetAngular/src/app/pagesweb/model/reponses.ts
+++ ProjetAngular/src/app/pagesweb/model/reponses.ts
@@ -1,5 +1,55 @@
+import { Environnement } from './environnement';
 import { Events } from './events';
+import { Objet } from './objet';
 export class Reponses {
+  public get objet(): Objet | undefined {
+    return this._objet;
+  }
+  public set objetId(value: Objet | undefined) {
+    this._objet = value;
+  }
+  public get environnement(): Environnement | undefined {
+    return this._environnement;
+  }
+  public set environnement(value: Environnement | undefined) {
+    this._environnement = value;
+  }
+  public get isAlive(): boolean | undefined {
+    return this._isAlive;
+  }
+  public set isAlive(value: boolean | undefined) {
+    this._isAlive = value;
+  }
+  public get ajoutCovid(): number | undefined {
+    return this._ajoutCovid;
+  }
+  public set ajoutCovid(value: number | undefined) {
+    this._ajoutCovid = value;
+  }
+  public get ajoutForce(): number | undefined {
+    return this._ajoutForce;
+  }
+  public set ajoutForce(value: number | undefined) {
+    this._ajoutForce = value;
+  }
+  public get ajoutEnergie(): number | undefined {
+    return this._ajoutEnergie;
+  }
+  public set ajoutEnergie(value: number | undefined) {
+    this._ajoutEnergie = value;
+  }
+  public get ajoutArgent(): number | undefined {
+    return this._ajoutArgent;
+  }
+  public set ajoutArgent(value: number | undefined) {
+    this._ajoutArgent = value;
+  }
+  public get ajoutPoids(): number | undefined {
+    return this._ajoutPoids;
+  }
+  public set ajoutPoids(value: number | undefined) {
+    this._ajoutPoids = value;
+  }
   public get prochainEvenementId(): Events | undefined {
     return this._prochainEvenementId;
   }
@@ -22,6 +72,14 @@ export class Reponses {
   constructor(
     private _id?: number,
     private _texte?: string,
-    private _prochainEvenementId?: Events
+    private _prochainEvenementId?: Events,
+    private _ajoutPoids?: number,
+    private _ajoutArgent?: number,
+    private _ajoutEnergie?: number,
+    private _ajoutForce?: number,
+    private _ajoutCovid?: number,
+    private _isAlive?: boolean,
+    private _environnement?: Environnement,
+    private _objet?: Objet
   ) {}
 }
